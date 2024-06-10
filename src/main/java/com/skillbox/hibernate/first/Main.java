f69a3e2db2f52f4cca5f63fe4b3469cd478c3ec7
OgarMikeDev <ogar.m.v@mail.ru>
2024-06-10 13:28:03 +0300

    hibernate

--- src/main/java/com/skillbox/hibernate/first/Main.java
+++ src/main/java/com/skillbox/hibernate/first/Main.java
@@ -16,6 +16,8 @@ public class Main {
     public static void main(String[] args) {
         Session session = sessionFactory.getCurrentSession();
         try {
+            User user = session.get(User.class, 1);
+            System.out.println("User: " + user);
 
 
 //            session.getTransaction().begin();
