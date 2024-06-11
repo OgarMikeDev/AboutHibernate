cfcbde08c823134999dca3d15e67cf35257ab08a
Mike Ogar <ogar.m.v@mail.ru>
2024-06-11 18:17:35 +0300

    hibernate

--- src/main/java/com/skillbox/hibernate/manyToOne/MainUniversity.java
+++ src/main/java/com/skillbox/hibernate/manyToOne/MainUniversity.java
@@ -22,50 +22,50 @@ public class MainUniversity {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
-//        //Университет МИФИ
-//        University university = new University();
-//        university.setTitle("МИФИ");
-//
-//        Student student = new Student();
-//        student.setUniversity(university);
-//        student.setName("Anna");
-//        session.save(student);
-//        university.setStudents(student);
-//
-//        Student studentTwo = new Student();
-//        studentTwo.setUniversity(university);
-//        studentTwo.setName("Boris");
-//        session.save(studentTwo);
-//        university.setStudents(studentTwo);
-//
-//        session.save(university);
-//
-//        //Университет МГУ им. Ломоносова
-//        University universityTwo = new University();
-//        universityTwo.setTitle("МГУ им. Ломоносова");
-//
-//        Student studentThree = new Student();
-//        studentThree.setUniversity(universityTwo);
-//        studentThree.setName("Air");
-//        session.save(studentThree);
-//        universityTwo.setStudents(studentThree);
-//
-//        Student studentFour = new Student();
-//        studentFour.setUniversity(universityTwo);
-//        studentFour.setName("Kiril");
-//        session.save(studentFour);
-//        universityTwo.setStudents(studentFour);
-//
-//        session.save(universityTwo);
-//
-//        System.out.println("First university: " + university.getStudents() + "\nSecond university: " + universityTwo.getStudents());
-        Student student = session.get(Student.class, 2);
-        Student student2 = session.get(Student.class, 3);
-        System.out.println(student + "\n" + student2);
+        //Университет МИФИ
+        University university = new University();
+        university.setTitle("МИФИ");
 
-        University university = session.get(University.class, 1);
-        University university2 = session.get(University.class, 2);
-        System.out.println(university + "\n" + university2);
+        Student student = new Student();
+        student.setUniversity(university);
+        student.setName("Anna");
+        session.save(student);
+        university.setStudents(student);
+
+        Student studentTwo = new Student();
+        studentTwo.setUniversity(university);
+        studentTwo.setName("Boris");
+        session.save(studentTwo);
+        university.setStudents(studentTwo);
+
+        session.save(university);
+
+        //Университет МГУ им. Ломоносова
+        University universityTwo = new University();
+        universityTwo.setTitle("МГУ им. Ломоносова");
+
+        Student studentThree = new Student();
+        studentThree.setUniversity(universityTwo);
+        studentThree.setName("Air");
+        session.save(studentThree);
+        universityTwo.setStudents(studentThree);
+
+        Student studentFour = new Student();
+        studentFour.setUniversity(universityTwo);
+        studentFour.setName("Kiril");
+        session.save(studentFour);
+        universityTwo.setStudents(studentFour);
+
+        session.save(universityTwo);
+
+        System.out.println("First university: " + university.getStudents() + "\nSecond university: " + universityTwo.getStudents());
+        Student updateStudent = session.get(Student.class, 3);
+        Student updateStudent2 = session.get(Student.class, 4);
+        System.out.println(updateStudent + "\n" + updateStudent2);
+
+        University updateUniversity = session.get(University.class, 1);
+        University updateUniversity2 = session.get(University.class, 2);
+        System.out.println(updateUniversity + "\n" + updateUniversity2);
 
         transaction.commit();
         sessionFactory.close();
