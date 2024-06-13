d0c98ccf6dc87634567ceee7d1e03b2195215d0a
OgarMikeDev <ogar.m.v@mail.ru>
2024-06-13 11:34:06 +0300

    hibernate

--- src/main/java/com/skillbox/hibernate/manyToOne/MainUniversity.java
+++ src/main/java/com/skillbox/hibernate/manyToOne/MainUniversity.java
@@ -22,50 +22,50 @@ public class MainUniversity {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
-        //Университет МИФИ
-        University university = new University();
-        university.setTitle("МИФИ");
-
-        Student student = new Student();
-        student.setUniversity(university);
-        student.setName("Anna");
-        session.save(student);
-        university.setStudents(student);
-
-        Student studentTwo = new Student();
-        studentTwo.setUniversity(university);
-        studentTwo.setName("Boris");
-        session.save(studentTwo);
-        university.setStudents(studentTwo);
-
-        session.save(university);
-
-        //Университет МГУ им. Ломоносова
-        University universityTwo = new University();
-        universityTwo.setTitle("МГУ им. Ломоносова");
-
-        Student studentThree = new Student();
-        studentThree.setUniversity(universityTwo);
-        studentThree.setName("Air");
-        session.save(studentThree);
-        universityTwo.setStudents(studentThree);
-
-        Student studentFour = new Student();
-        studentFour.setUniversity(universityTwo);
-        studentFour.setName("Kiril");
-        session.save(studentFour);
-        universityTwo.setStudents(studentFour);
-
-        session.save(universityTwo);
-
-        System.out.println("First university: " + university.getStudents() + "\nSecond university: " + universityTwo.getStudents());
-//        Student updateStudent = session.get(Student.class, "Anna");
-//        Student updateStudent2 = session.get(Student.class, "Air");
-//        System.out.println(updateStudent + "\n" + updateStudent2);
+//        //Университет МИФИ
+//        University university = new University();
+//        university.setTitle("МИФИ");
 //
-//        University updateUniversity = session.get(University.class, "МИФИ");
-//        University updateUniversity2 = session.get(University.class, "МГУ им. Ломоносова");
-//        System.out.println(updateUniversity + "\n" + updateUniversity2);
+//        Student student = new Student();
+//        student.setUniversity(university);
+//        student.setName("Anna");
+//        session.save(student);
+//        university.setStudents(student);
+//
+//        Student studentTwo = new Student();
+//        studentTwo.setUniversity(university);
+//        studentTwo.setName("Boris");
+//        session.save(studentTwo);
+//        university.setStudents(studentTwo);
+//
+//        session.save(university);
+//
+//        //Университет МГУ им. Ломоносова
+//        University universityTwo = new University();
+//        universityTwo.setTitle("МГУ им. Ломоносова");
+//
+//        Student studentThree = new Student();
+//        studentThree.setUniversity(universityTwo);
+//        studentThree.setName("Air");
+//        session.save(studentThree);
+//        universityTwo.setStudents(studentThree);
+//
+//        Student studentFour = new Student();
+//        studentFour.setUniversity(universityTwo);
+//        studentFour.setName("Kiril");
+//        session.save(studentFour);
+//        universityTwo.setStudents(studentFour);
+//
+//        session.save(universityTwo);
+//
+//        System.out.println("First university: " + university.getStudents() + "\nSecond university: " + universityTwo.getStudents());
+
+        Student updateStudent = session.get(Student.class, "Anna");
+        Student updateStudent2 = session.get(Student.class, "Air");
+        System.out.println(updateStudent + "\n" + updateStudent2);
+        University updateUniversity = session.get(University.class, "МИФИ");
+        University updateUniversity2 = session.get(University.class, "МГУ им. Ломоносова");
+        System.out.println(updateUniversity + "\n" + updateUniversity2);
 
         transaction.commit();
         sessionFactory.close();
